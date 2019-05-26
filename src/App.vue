0fbf4eb40f1477f1ae0ebc9cb47f74d7a16c552d
Leandro Araujo <laraujo@MacBook-Pro-de-Leandro.local>
2019-05-26 18:59:24 -0400

    Tema Atualizado

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav>
+      <div class="nav-wrapper container">
+        <a href="#" class="brand-logo">VueJS Example</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Sass</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li>
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col s12">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  styles: [
+    require('materialize-css/dist/css/materialize.min.css'),
+    require('ionicons/dist/css/ionicons.min.css')
+  ]
 }
 </script>
 
@@ -16,8 +41,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
